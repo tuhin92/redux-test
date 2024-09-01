c85c510cce7896b9d8c1e950ce92b169a1d738ae
tuhin92 <tuhinalmamun71m@gmail.com>
2024-09-01 19:42:40 +0600

    create slice

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
-
+import counterRrducer from './features/counter/counterSlice';
 export const store = configureStore({
-    reducer: {},
+    reducer: {
+        counter: counterRrducer,
+    },
 })
